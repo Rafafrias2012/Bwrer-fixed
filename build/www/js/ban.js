26be57f365a4d3ce5abcd9a1a110e123f35f5e2e
ItzRafaelScout <155685529+Rafafrias2012@users.noreply.github.com>
2024-12-14 14:04:43 -0300

    Initial commit

---
+++ build/www/js/ban.js
@@ -0,0 +1,67 @@
+function ban() {
+	var x = document.getElementById("banmenu_reason").value;
+	var x2 = document.getElementById("banmenu_ip").value;
+	var x3 = document.getElementById("banmenu_end").value;
+	socket.emit("command", {list:["ban",x2,x]});
+	var x4 = document.getElementById("page_banmenu");
+	if (x4.style.display === "none") {
+		x4.style.display = "block";
+	} else {
+		x4.style.display = "none";
+	}
+	alert("Banned!") 
+}
+
+function fuckoff() {
+	var x = document.getElementById("fomenu_reason").value;
+	var x2 = document.getElementById("fomenu_ip").value;
+	var x3 = document.getElementById("fomenu_end").value;
+	socket.emit("command", {list:["fuckoffban",x2,x]});
+	var x4 = document.getElementById("page_fomenu");
+	if (x4.style.display === "none") {
+		x4.style.display = "block";
+	} else {
+		x4.style.display = "none";
+	}
+	alert("YFM'd!") 
+}
+
+function report() {
+	var x = document.getElementById("reportmenu_reason").value;
+	var x2 = document.getElementById("reportmenu_name").value;
+	socket.emit("command", {list:["report",x2,x]});
+	var x4 = document.getElementById("page_reportmenu");
+	if (x4.style.display === "none") {
+		x4.style.display = "block";
+	} else {
+		x4.style.display = "none";
+	}
+	alert("Banned!") 
+}
+
+function banmenu() {
+	var x4 = document.getElementById("page_banmenu");
+	if (x4.style.display === "none") {
+		x4.style.display = "block";
+	} else {
+		x4.style.display = "none";
+	}
+}
+
+function fuckoffmenu() {
+	var x4 = document.getElementById("page_fomenu");
+	if (x4.style.display === "none") {
+		x4.style.display = "block";
+	} else {
+		x4.style.display = "none";
+	}
+}
+
+function reportmenu() {
+	var x4 = document.getElementById("page_reportmenu");
+	if (x4.style.display === "none") {
+		x4.style.display = "block";
+	} else {
+		x4.style.display = "none";
+	}
+}
